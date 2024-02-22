7161ea7c79bf83345635d3494d46ebc749a2cf76
gomez-a443 <114755613+gomez-a443@users.noreply.github.com>
2024-02-22 13:03:01 +0530

    Revert "connect to the database"

--- api/index.js
+++ api/index.js
@@ -1,18 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
-mongoose
-  .connect(
-    "mongodb+srv://aparna:aparna@mern-blog.zzieoey.mongodb.net/mern-blog?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("mongo db is connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000!!");
